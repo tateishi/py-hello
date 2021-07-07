9295140e62d7c238104f69e19d5a8cc040515c82
Tateishi Tadatoshi <ishio39@gmail.com>
2021-07-07 18:28:14 +0900

    add say_hello

--- hello/__init__.py
+++ hello/__init__.py
@@ -4,3 +4,4 @@ VERSION = '0.0.1'
 from . import core
 
 greeting = core.greeting
+say_hello = core.say_hello
--- hello/core.py
+++ hello/core.py
@@ -1,4 +1,11 @@
+import sys
+from typing import TextIO
 
 def greeting(name: str = 'World') -> str:
     """Returns greeting message."""
     return f'Hello {name}'
+
+
+def say_hello(name: str = 'World', file: TextIO = sys.stdout) -> str:
+    """Print greeting message."""
+    print(greeting(name), file=file)
